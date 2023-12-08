c174eb1a7b4dc11152839dac6a3a81df8dcc48c8
arqoofficial <argolubev97@yandex.ru>
2023-12-08 09:58:36 +0300

    fix: fix run_client.sh

--- client/run_client.sh
+++ client/run_client.sh
@@ -1,1 +1,1 @@
-streamlit run /src/client/unitogo.py --server.port=8501 --server.address=0.0.0.0 --server.enableCORS false
+streamlit run /app/client/unitogo.py --server.port=8501 --server.address=0.0.0.0 --server.enableCORS false
